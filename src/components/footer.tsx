d4681aee9b66e0984fa8541650a0848d359209e2
Max Schmidt <max.schmidt@outlook.de>
2022-01-07 21:53:20 +0100

    link to github
    Signed-off-by: Max Schmidt <max.schmidt@outlook.de>

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,10 +1,15 @@
 import Link from 'next/link';
+import Image from 'next/image';
+import github from '../../public/pixels/github.png';
 
 const Footer = () => (
-	<div className="col-span-2 lg:col-span-1 flex justify-between">
+	<div className="col-span-2 lg:col-span-1 flex items-center justify-between">
 		<Link href="/impressum">
 			<a>Impressum</a>
 		</Link>
+		<a className="relative w-3" href="https://github.com/mooxl" target="_blank">
+			<Image src={github} />
+		</a>
 		<Link href="/datenschutz">
 			<a>Datenschutz</a>
 		</Link>
