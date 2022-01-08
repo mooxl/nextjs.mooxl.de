2bf54cc9a42703aab47b32fd2188099e10e0ba0a
Max Schmidt <max.schmidt@outlook.de>
2022-01-08 12:49:00 +0100

    better SEO
    Signed-off-by: Max Schmidt <max.schmidt@outlook.de>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
 import Head from 'next/head';
+import Script from 'next/script';
 import '../utils/globals.css';
 
 const App = ({ Component, pageProps }) => (
 	<>
 		<Head>
 			<title>Max Schmidt - Full Stack Web Developer</title>
-			<meta name="description" content="" />
+			<meta
+				name="description"
+				content="Full Stack Web Developer and TypeScript enthusiast"
+			/>
 			<meta charSet="utf-8" />
 			<meta name="viewport" content="initial-scale=1.0, width=device-width" />
 		</Head>
