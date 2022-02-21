aedf729a1868db861f848f38cefb0c849d4a5959
Max Schmidt <max.schmidt@outlook.de>
2022-02-21 23:22:14 +0100

    use preact in production
    Signed-off-by: Max Schmidt <max.schmidt@outlook.de>

--- next.config.js
+++ next.config.js
@@ -3,4 +3,17 @@ module.exports = {
 		locales: ['de-DE'],
 		defaultLocale: 'de-DE',
 	},
+	swcMinify: true,
+	reactStrictMode: true,
+	webpack: (config, { dev, isServer }) => {
+		// Replace React with Preact only in client production build
+		if (!dev && !isServer) {
+			Object.assign(config.resolve.alias, {
+				'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
+				react: 'preact/compat',
+				'react-dom/test-utils': 'preact/test-utils',
+				'react-dom': 'preact/compat',
+			});
+		}
+	},
 };
