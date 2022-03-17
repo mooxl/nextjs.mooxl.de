403deca57f64fdde75cd5516717d5a8d1faf9b27
Max Schmidt <max.schmidt@outlook.de>
2022-03-17 19:53:55 +0100

    new datenschutz
    Signed-off-by: Max Schmidt <max.schmidt@outlook.de>

--- src/pages/datenschutz.tsx
+++ src/pages/datenschutz.tsx
@@ -2,135 +2,276 @@ const Impressum = () => (
 	<div className="space-y-2">
 		<h1>Datenschutz</h1>
 		<p>
-			Wir haben diese Datenschutzerklärung verfasst, um Ihnen gemäß der Vorgaben
-			der DSGVO zu erklären, welche Informationen wir sammeln, wie wir Daten
-			verwenden und welche Entscheidungsmöglichkeiten Sie als Besucher dieser
-			Webseite haben. Leider liegt es in der Natur der Sache, dass diese
-			Erklärungen sehr technisch klingen, wir haben uns bei der Erstellung
-			jedoch bemüht die wichtigsten Dinge so einfach und klar wie möglich zu
-			beschreiben.
+			Der Schutz Ihrer persönlichen Daten ist uns wichtig. Daher möchten wir Sie
+			an dieser Stelle informieren, wie wir die Datenschutzbestimmungen im
+			Rahmen unseres Internet-Angebotes umsetzen.
 		</p>
-		<h2>Automatische Datenspeicherung</h2>
+		<h2>1. Name und Kontaktdaten des für die Verarbeitung Verantwortlichen</h2>
 		<p>
-			Wenn Sie heutzutage Webseiten besuchen, werden gewisse Informationen
-			automatisch erstellt und gespeichert, so auch auf dieser Webseite. Wenn
-			Sie unsere Webseite so wie jetzt gerade besuchen, speichert unser
-			Webserver (Computer auf dem diese Webseite gespeichert ist) automatisch
-			Daten, wie:
+			Diese Datenschutz-Information gilt für die Datenverarbeitung durch
+			nachfolgend angegebene verantwortliche Stelle:
+			<br />
+			<br />
+			Max Schmidt <br />
+			Gutenbergstraße 38 <br />
+			50823 Köln <br />
+			Deutschland <br />
+			<br />
+			E-Mail: datenschutz@mooxl.de <br />
 		</p>
-		<ul className="list-disc list-inside mb-3">
-			<li>die Adresse (URL) der aufgerufenen Webseite</li>
-			<li>Browser und Browserversion - das verwendete Betriebssystem</li>
-			<li>die Adresse (URL) der zuvor besuchten Seite (Referrer URL)</li>
+		<h2>2. Erfassung allgemeiner Informationen beim Besuch unserer Website</h2>
+		<p>
+			Wenn Sie auf unsere Website zugreifen, werden automatisch Informationen
+			allgemeiner Natur erfasst. Diese Informationen (Server-Logfiles)
+			beinhalten etwa die Art des Webbrowsers, das verwendete Betriebssystem,
+			den Domainnamen Ihres Internet-Service-Providers und ähnliches. Hierbei
+			handelt es sich ausschließlich um anonymisierte Informationen, welche
+			keine Rückschlüsse auf Ihre Person zulassen.
+			<br />
+			<br />
+			Diese Informationen sind technisch notwendig, um von Ihnen angeforderte
+			Inhalte von Webseiten korrekt auszuliefern und fallen bei Nutzung des
+			Internets zwingend an. Sie werden insbesondere zu folgenden Zwecken
+			verarbeitet
+		</p>
+		<ul className="my-1 list-disc list-inside">
+			<li>Sicherstellung eines reibungslosen Verbindungsaufbaus der Website</li>
 			<li>
-				den Hostname und die IP-Adresse des Geräts von welchem aus zugegriffen
-				wird
+				Sicherstellung einer möglichst komfortablen Nutzung unserer Website
 			</li>
-			<li>Datum und Uhrzeit in Dateien (Webserver-Logfiles)</li>
+			<li>Auswertung der Systemsicherheit und -stabilität sowie</li>
+			<li>zu weiteren administrativen Zwecken.</li>
 		</ul>
 		<p>
-			In der Regel werden Webserver-Logfiles zwei Wochen gespeichert und danach
-			automatisch gelöscht. Wir geben diese Daten nicht weiter, können jedoch
-			nicht ausschließen, dass diese Daten beim Vorliegen von rechtswidrigem
-			Verhalten eingesehen werden. Die Rechtsgrundlage besteht nach Artikel 6
-			Absatz 1 f DSGVO (Rechtmäßigkeit der Verarbeitung) darin, dass
-			berechtigtes Interesse daran besteht, den fehlerfreien Betrieb dieser
-			Webseite durch das Erfassen von Webserver-Logfiles zu ermöglichen.
+			Anonyme Informationen dieser Art werden von uns ggf. statistisch
+			ausgewertet, um unseren Internetauftritt und die dahinterstehende Technik
+			zu optimieren.
+			<br />
+			<br />
+			Rechtsgrundlage für die Datenverarbeitung ist Art. 6 Abs. 1 S. 1 lit. f
+			der Datenschutz-Grundverordnung (EU-Verordnung 2016/679) (nachfolgend
+			„DSGVO“). Unser berechtigtes Interesse folgt aus oben aufgelisteten
+			Zwecken zur Datenerhebung. In keinem Fall verwenden wir die erhobenen
+			Daten zu dem Zweck, Rückschlüsse auf Ihre Person zu ziehen. Empfänger der
+			Daten sind nur die angegebene verantwortliche Stelle und ggf.
+			Auftragsverarbeiter.
 		</p>
-		<h2>Speicherung persönlicher Daten</h2>
+		<h2>3. Cookies</h2>
 		<p>
-			Persönliche Daten, die Sie uns auf dieser Website elektronisch
-			übermitteln, wie zum Beispiel Name, E-Mail-Adresse, Adresse oder andere
-			persönlichen Angaben im Rahmen der Übermittlung eines Formulars, werden
-			von uns gemeinsam mit dem Zeitpunkt und der IP-Adresse nur zum jeweils
-			angegebenen Zweck verwendet, sicher verwahrt und nicht an Dritte
-			weitergegeben.
+			Im Gegensatz zu anderen Seiten, verwenden wir überhaupt keine Cookies.
+			Deswegen sehen Sie auch keinen Cookie-Consent-Banner auf unserer Website.
 			<br />
 			<br />
-			Wir nutzen Ihre persönlichen Daten somit nur für die Kommunikation mit
-			jenen Besuchern, die Kontakt ausdrücklich wünschen und für die Abwicklung
-			der auf dieser Webseite angebotenen Dienstleistungen und Produkte. Wir
-			geben Ihre persönlichen Daten ohne Zustimmung nicht weiter, können jedoch
-			nicht ausschließen, dass diese Daten beim Vorliegen von rechtswidrigem
-			Verhalten eingesehen werden. Wenn Sie uns persönliche Daten per E-Mail
-			schicken – somit abseits dieser Webseite – können wir keine sichere
-			Übertragung und den Schutz Ihrer Daten garantieren. Wir empfehlen Ihnen,
-			vertrauliche Daten niemals unverschlüsselt per E-Mail zu übermitteln.
+			Cookies sind kleine Textdateien, die von einem Websiteserver auf Ihr
+			Endgerät (Laptop, Tablet, Smartphone o.ä.) übertragen und dort gespeichert
+			werden. Hierdurch erhalten wir automatisch bestimmte Daten wie z.B.
+			IP-Adresse, verwendeter Browser, Betriebssystem und Ihre Verbindung zum
+			Internet.
 			<br />
 			<br />
-			Die Rechtsgrundlage besteht nach Artikel 6 Absatz 1 a DSGVO
-			(Rechtmäßigkeit der Verarbeitung) darin, dass Sie uns die Einwilligung zur
-			Verarbeitung der von Ihnen eingegebenen Daten geben. Sie können diesen
-			Einwilligung jederzeit widerrufen – eine formlose E-Mail reicht aus, Sie
-			finden unsere Kontaktdaten im Impressum. Rechte laut
-			Datenschutzgrundverordnung
+			Cookies können nicht verwendet werden, um Programme zu starten oder Viren
+			oder sonstige Schadsoftware auf Ihr Endgerät zu übertragen. Anhand der in
+			Cookies enthaltenen Informationen können wir Ihnen die Navigation
+			erleichtern und die korrekte Anzeige unserer Webseiten ermöglichen. Der
+			Einsatz von Cookies dient einerseits dazu, die Nutzung des Angebots für
+			Sie angenehmer zu gestalten. So können sogenannte Session-Cookies
+			eingesetzt werden, um zu erkennen, dass Sie einzelne Seiten einer Website
+			bereits besucht haben. Diese werden nach Verlassen unserer Seite
+			normalerweise automatisch gelöscht. Darüber hinaus können ebenfalls zur
+			Optimierung der Benutzerfreundlichkeit temporäre Cookies benutzt werden,
+			die für einen bestimmten festgelegten Zeitraum auf Ihrem Endgerät
+			gespeichert werden. Besuchen Sie dieseSeite erneut, um unsere Dienste in
+			Anspruch zu nehmen, wird automatisch erkannt, dass Sie bereits auf der
+			Website waren und welche Eingaben und Einstellungen sie getätigt haben, um
+			diese nicht noch einmal eingeben zu müssen.
 			<br />
 			<br />
-			Ihnen stehen laut den Bestimmungen der DSGVO die folgende Rechte zu:
+			In keinem Fall werden die von uns erfassten Daten an Dritte weitergegeben
+			oder ohne Ihre Einwilligung eine Verknüpfung mit personenbezogenen Daten
+			hergestellt.
+			<br />
+			<br />
+			Sie können alle Websites grundsätzlich auch ohne Cookies betrachten.
+			Internet-Browser sind regelmäßig so eingestellt, dass sie Cookies
+			akzeptieren. Im Allgemeinen können Sie die Verwendung von Cookies
+			jederzeit über die Einstellungen Ihres Browsers deaktivieren. Bitte
+			verwenden Sie die Hilfefunktionen Ihres Internetbrowsers, um zu erfahren,
+			wie Sie diese Einstellungen ändern können. Bitte beachten Sie, dass
+			einzelne Funktionen unserer Website möglicherweise nicht funktionieren,
+			wenn Sie die Verwendung von Cookies deaktiviert haben.
+			<br />
+			<br />
+			Die durch Cookies verarbeiteten Daten sind für die genannten Zwecke zur
+			Wahrung unserer berechtigten Interessen sowie der Dritter nach Art. 6 Abs.
+			1 S. 1 lit. f DSGVO erforderlich.
 		</p>
-		<ul className="list-disc list-inside mb-3">
-			<li>Recht auf Berichtung (Artikel 16 DSGVO)</li>
-			<li>
-				Recht auf Löschung („Recht auf Vergessenwerden“) (Artikel 17 DSGVO)
-			</li>
-			<li>Recht auf Einschränkung der Verarbeitung (Artikel 18 DSGVO)</li>
-			<li>
-				Recht auf Benachrichtigung – Mitteilungspflicht im Zusammenhang mit der
-				Berichtigung oder Löschung personenbezogener Daten oder der
-				Einschränkung der Verarbeitung (Artikel 19 DSGVO)
-			</li>
-			<li>Recht auf Datenübertragbarkeit (Artikel 20 DSGVO)</li>
-			<li>Widerspruchsrecht (Artikel 21 DSGVO)</li>
-			<li>
-				Recht, nicht einer ausschließlich auf einer automatisierten Verarbeitung
-				— einschließlich Profiling — beruhenden Entscheidung unterworfen zu
-				werden (Artikel 22 DSGVO)
-			</li>
-		</ul>
-		<h2>TLS-Verschlüsselung mit https</h2>
+		<h2>4. Analyse- und Tracking-Tools</h2>
 		<p>
-			Wir verwenden https um Daten abhörsicher im Internet zu übertragen
-			(Datenschutz durch Technikgestaltung Artikel 25 Absatz 1 DSGVO). Durch den
-			Einsatz von TLS (Transport Layer Security), einem
-			Verschlüsselungsprotokoll zur sicheren Datenübertragung im Internet können
-			wir den Schutz vertraulicher Daten sicherstellen. Sie erkennen die
-			Benutzung dieser Absicherung der Datenübertragung am kleinen Schlosssymbol
-			links oben im Browser und der Verwendung des Schemas https (anstatt http)
-			als Teil
-		</p>
-		<h2>Cookies</h2>
-		<p>
-			Unsere Website verwendet lediglich HTTP-Cookies um den reibungslosen
-			Betrieb der Website zu gewährleisten. Nutzerspezifische Daten vie Cookies
-			werden nicht von unserer Website gespeichert. Ein Cookie ist ein kurzes
-			Datenpaket, welches zwischen Webbrowser und Webserver ausgetauscht wird,
-			für diese aber völlig bedeutungslos ist und erst für die Webanwendung, z.
-			B. einen Online-Shop, eine Bedeutung erhält, etwa den Inhalt eines
-			virtuellen Warenkorbes.
+			Die im Folgenden aufgeführte und von uns eingesetzte Analyse- und
+			Tracking-Maßnahme (nachfolgend „Maßnahme“) wird auf Grundlage des Art. 6
+			Abs. 1 S. 1 lit. f DSGVO durchgeführt. Mit der zum Einsatz kommenden
+			Maßnahme wollen wir eine bedarfsgerechte Gestaltung und die fortlaufende
+			Optimierung unserer Webseite sicherstellen. Zum anderen setzen wir die
+			Maßnahme ein, um die Nutzung unserer Webseite statistisch zu erfassen und
+			zum Zwecke der Optimierung unseres Webangebotes für Sie auszuwerten. Diese
+			Interessen sind als berechtigt im Sinne der vorgenannten Vorschrift
+			anzusehen.
 			<br />
 			<br />
-			Es gibt zwei Arten von Cookies: Erstanbieter-Cookies werden von unserer
-			Website erstellt, Drittanbieter-Cookies werden von anderen Websites (z. B.
-			Google Analytics) erstellt. Man unterscheidet drei Kategorien von Cookies:
-			unbedingt notwendige Cookies um grundlegende Funktionen der Website
-			sicherzustellen, funktionelle Cookies um die Leistung der Webseite
-			sicherzustellen und zielorientierte Cookies um das Benutzererlebnis zu
-			verbessern.
+			Die jeweiligen Datenverarbeitungszwecke und Datenkategorien sind den
+			nachfolgenden Informationen zu entnehmen.
 			<br />
 			<br />
-			Wir nutzen Cookies, um unsere Webseite nutzerfreundlicher zu gestalten.
-			Einige Cookies bleiben auf Ihrem Endgerät gespeichert, bis Sie diese
-			löschen. Sie ermöglichen es uns, Ihren Browser beim nächsten Besuch
-			wiederzuerkennen.
-		</p>
-		<p>
-			Quelle: Erstellt mit dem "
-			<a
-				href="https://www.adsimple.de/datenschutz-generator/"
-				title="Datenschutz Generator von AdSimple für Deutschland">
-				Datenschutz Generator
+			<br />
+			<strong>Umami</strong> <br />
+			<br />
+			Zu Analyse der Reichweite meiner Website benutzen wir Umami. Der Schutz
+			Ihrer Daten ist uns wichtig. Deshalb erfasst Umami keine IP Adresse und
+			speichert keine Informationen auf Ihrem Endgerät (z.B. Cookies). Wir
+			verarbeiten Ihre personenbezogenen Nutzungsdaten nur anonymisiert. Ein
+			Rückschluss auf Ihre Person ist uns nicht möglich. Darüber hinaus
+			betreiben wir Umami auf unseren eigenen Servern, weswegen eine
+			Weiterleitung der Daten an Dritte nicht erfolgt. Weitere Informationen
+			finden Sie in der Dokumentation für Umami Analytics unter:{' '}
+			<a href="https://umami.is/" target="_blank">
+				https://umami.is
 			</a>
-			" von AdSimple
+			 .
+		</p>
+		<h2>5. Kontaktformular</h2>
+		<p>
+			Treten Sie bzgl. Fragen jeglicher Art per E-Mail oder Kontaktformular mit
+			uns in Kontakt, erteilen Sie uns zum Zwecke der Kontaktaufnahme Ihre
+			freiwillige Einwilligung. Hierfür ist die Angabe einer validen
+			E-Mail-Adresse erforderlich. Diese dient der Zuordnung der Anfrage und der
+			anschließenden Beantwortung derselben. Die von Ihnen gemachten Angaben
+			werden zum Zwecke der Bearbeitung der Anfrage sowie für mögliche
+			Anschlussfragen gespeichert. Nach Erledigung der von Ihnen gestellten
+			Anfrage werden personenbezogene Daten automatisch gelöscht.
+		</p>
+		<h2>6. Zwecke der Datenverarbeitung, Weitergabe von Daten</h2>
+		<p>
+			Wir verarbeiten Ihre personenbezogenen Daten nur zu den in dieser
+			Datenschutzerklärung genannten Zwecken. Eine Übermittlung Ihrer
+			persönlichen Daten an Dritte zu anderen als den genannten Zwecken findet
+			nicht statt.
+			<br />
+			<br />
+			Wir geben Ihre persönlichen Daten nur an Dritte weiter, wenn:
+		</p>
+		<ul className="my-1 list-disc list-inside">
+			<li>
+				Sie gem. Art. 6 Abs. 1 S. 1 lit. a DSGVO Ihre ausdrückliche Einwilligung
+				dazu erteilt haben,
+			</li>
+			<li>
+				die Verarbeitung gem. Art. 6 Abs. 1 S. 1 lit. b DSGVO zur Abwicklung
+				eines Vertrages mit Ihnen erforderlich ist,
+			</li>
+			<li>
+				die Verarbeitung gem. Art. 6 Abs. 1 S. 1 lit. c DSGVO zur Erfüllung
+				einer rechtlichen Verpflichtung erforderlich ist oder
+			</li>
+			<li>
+				die Verarbeitung gem. Art. 6 Abs. 1 S. 1 lit. f DSGVO zur Wahrung
+				berechtigter Interessen erforderlich ist und kein Grund zur Annahme
+				besteht, dass Sie ein überwiegendes schutzwürdiges Interesse an der
+				Nichtweitergabe Ihrer Daten haben.
+			</li>
+		</ul>
+		<h2>7. Löschung und Sperrung von Daten</h2>
+		<p>
+			Wir halten uns an die Grundsätze der Datenvermeidung und Datensparsamkeit.
+			Wir speichern Ihre personenbezogenen Daten daher nur so lange, wie dies
+			zur Erreichung der hier genannten Zwecke erforderlich ist oder wie es die
+			von Gesetz- oder Verordnungsgebern vorgesehenen Speicherfristen vorsehen.
+			Nach Fortfall des jeweiligen Zweckes bzw. Ablauf dieser Fristen werden die
+			entsprechenden Daten routinemäßig und gemäß den rechtlichen Vorgaben
+			gesperrt oder gelöscht.
+		</p>
+		<h2>8. Betroffenenrechte</h2>
+		<p>Sie können jederzeit folgende Rechte ausüben:</p>
+		<ul className="my-1 list-disc list-inside">
+			<li>
+				gemäß Art. 15 DSGVO Auskunft über Ihre bei uns gespeicherten Daten und
+				deren Verarbeitung;
+			</li>
+			<li>
+				gemäß Art. 16 DSGVO Berichtigung unrichtiger personenbezogener Daten;
+			</li>
+			<li>
+				gemäß Art. 17 DSGVO Löschung Ihrer bei uns gespeicherten Daten, soweit
+				nicht die Verarbeitung zur Ausübung des Rechts auf freie
+				Meinungsäußerung und Information, zur Erfüllung einer rechtlichen
+				Verpflichtung, aus Gründen des öffentlichen Interesses oder zur
+				Geltendmachung, Ausübung oder Verteidigung von Rechtsansprüchen
+				erforderlich ist;
+			</li>
+			<li>
+				gemäß Art. 18 DSGVO Einschränkung der Datenverarbeitung, sofern wir Ihre
+				Daten aufgrund gesetzlicher Pflichten noch nicht löschen dürfen;
+			</li>
+			<li>
+				gemäß Art. 20 DSGVO Datenübertragbarkeit, sofern Sie in die
+				Datenverarbeitung eingewilligt haben oder einen Vertrag mit uns
+				abgeschlossen haben;
+			</li>
+			<li>
+				gemäß Art. 21 DSGVO Widerspruch gegen die Verarbeitung Ihrer Daten bei
+				uns, soweit dafür Gründe vorliegen, die sich aus Ihrer besonderen
+				Situation ergeben oder sich der Widerspruch gegen Direktwerbung richtet.
+				Im letzteren Fall haben Sie ein generelles Widerspruchsrecht, das ohne
+				Angabe einer besonderen Situation von uns umgesetzt wird.
+			</li>
+		</ul>
+		<p>
+			Sofern Sie uns eine Einwilligung erteilt haben, können Sie diese jederzeit
+			mit Wirkung für die Zukunft widerrufen.
+			<br />
+			Möchten Sie von Ihrem Widerrufs- oder Widerspruchsrecht Gebrauch machen,
+			kontaktieren Sie uns bitte per E-Mail unter unserer in Ziffer 1.
+			angegebenen E-Mail-Adresse.
+			<br />
+			Sie können sich jederzeit mit einer Beschwerde an die für Sie zuständige
+			Aufsichtsbehörde wenden. Ihre zuständige Aufsichtsbehörde richtet sich
+			nach dem Bundesland Ihres Wohnsitzes, Ihrer Arbeit oder der mutmaßlichen
+			Verletzung. Eine Liste der Aufsichtsbehörden (für den nichtöffentlichen
+			Bereich) mit Anschrift finden Sie unter:
+			<a
+				href="https://www.bfdi.bund.de/DE/Infothek/Anschriften_Links/anschriften_links-node.html"
+				target="_blank">
+				https://www.bfdi.bund.de/DE/Infothek/Anschriften_Links/anschriften_links-node.html
+			</a>
+		</p>
+		<h2>9. Datensicherheit</h2>
+		<p>
+			Wir bedienen uns geeigneter technischer und organisatorischer
+			Sicherheitsmaßnahmen, um Ihre Daten gegen zufällige oder vorsätzliche
+			Manipulationen, teilweisen oder vollständigen Verlust, Zerstörung oder
+			gegen den unbefugten Zugriff Dritter zu schützen. Um dies zu
+			gewährleisten, verwenden wir beispielsweise dem aktuellen Stand der
+			Technik entsprechende Verschlüsselungsverfahren (z.B. SSL). Unsere
+			Sicherheitsmaßnahmen werden entsprechend der technologischen Entwicklung
+			fortlaufend verbessert.
+		</p>
+		<h2>10. Aktualität und Änderung dieser Datenschutzerklärung, Fragen</h2>
+		<p>
+			Diese Datenschutzerklärung ist aktuell gültig und hat den Stand März 2022.
+			<br />
+			<br />
+			Wir behalten uns vor, diese Datenschutzerklärung anzupassen, damit sie
+			stets den aktuellen rechtlichen Anforderungen entspricht oder um
+			Änderungen unserer Leistungen in der Datenschutzerklärung umzusetzen, z.B.
+			bei der Einführung neuer Services. Für Ihren erneuten Besuch gilt dann die
+			neue Datenschutzerklärung.
+			<br />
+			<br />
+			Bitte zögern Sie nicht, bei ergänzenden Fragen zum Thema Datenschutz mit
+			uns Kontakt aufzunehmen. Unsere Kontaktdaten ersehen Sie bitte aus Ziff. 1
+			dieser Datenschutzerklärung.
+			<br />
+			<br />
+			Stand: 16.03.2022
 		</p>
 	</div>
 );
